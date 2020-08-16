54e0d1b521ed3cb9e83eebd5feb080d5aced6507
Prajjwol <prajjwolshrestha@icloud.com>
2020-08-16 13:10:48 +1000

    added ref to clear input

--- App.js
+++ App.js
@@ -33,7 +33,10 @@ export default class App extends Component {
           style={styles.input}
           placeholder="$ amount" 
           onChangeText={ text => this.setState({expenseAmount: parseFloat(text) }) } 
-          keyboardType="number-pad" />
+          keyboardType="number-pad"
+          ref={(input)=> (this._textInput = input )}
+          
+          />
         {/* <TextInput
           style={styles.input}
           placeholder="category"
@@ -42,10 +45,13 @@ export default class App extends Component {
 
 
         <RNPickerSelect
+          
           items = {this.dropdownItems}
           value = {this.state.expenseCategory}
           onValueChange = { value => this.setState({expenseCategory: value})}
           useNativeAndroidPickerStyle = {false}
+          style = {pickerStyle}
+          placeholder={pickerPlaceholder}
         />
 
         </View>
@@ -85,12 +91,20 @@ export default class App extends Component {
     this.listData.push(listItem)
     console.log('adding')
     // this.setState({updating: true})
-    this.setState({expenseAmount:0})
+    this.setState({expenseAmount:0,expenseCategory:null})
+    this._textInput.clear()
+    this._textInput.focus()
   }
 }
 const colors = {
   primary : 'hsla(330, 38%, 65%, 1)'
 }
+
+const pickerPlaceholder = {
+  label: 'select category', value: null, color:'black'
+}
+
+
 const styles = StyleSheet.create({
   main: {
     paddingHorizontal: 10,
@@ -109,5 +123,18 @@ const styles = StyleSheet.create({
   buttonText: {
     color: 'white',
     textAlign: 'center'
+  }
+})
+
+const pickerStyle = StyleSheet.create({
+  inputIOS: {
+    padding: 10,
+    borderColor: colors.primary,
+    borderWidth: 1,
+  },
+  inputAndroid: {
+    padding: 10,
+    borderColor: colors.primary,
+    borderWidth: 1,
   }
 })
