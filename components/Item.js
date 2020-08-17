2593a0b703299b3d74d04d9d0c10ef0f3524171a
Prajjwol <prajjwolshrestha@icloud.com>
2020-08-17 18:04:23 +1000

    added delete icon

--- components/Item.js
+++ components/Item.js
@@ -1,14 +1,21 @@
 import React from 'react';
-import {Text,View,StyleSheet} from 'react-native';
+import {Text,View,StyleSheet,TouchableOpacity,Image} from 'react-native';
 
+//use touchable opacity as a button instead of using button
 
 //create a function
 export const Item = (props) => {
     //return a view for Item
     return (
         <View style={itemStyles.item}>
-            <Text style={itemStyles.text}>{props.category}</Text>
-            <Text style={itemStyles.text}>{props.amount}</Text>
+            <View style={itemStyles.row}>
+                <Text style={itemStyles.text}>{props.category}</Text>
+                <Text style={itemStyles.text}>{props.amount}</Text>
+            </View>
+
+            <TouchableOpacity>
+                <Image style={itemStyles.icon} source={require('../assets/trash-alt-solid.png')}/>  
+            </TouchableOpacity>
         </View>
     )
 }
@@ -21,12 +28,23 @@ const itemStyles = StyleSheet.create({
         padding:10,
         display:'flex',
         flexDirection:'row',
-        justifyContent:'space-between'
+        justifyContent:'space-between',
     },
     text:{
         fontSize:16,
         color:'black'
+    },
+    row:{
+        flexDirection:'row',
+        flex:1,
+        justifyContent:'space-between',
+        paddingRight:10,
+    },
+    icon:{
+        width:20,
+        height:20,
     }
+
 })
 
 /*
